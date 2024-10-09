a3eb6ad04e5eef0df95dbed5c01a6fd16490d6bc
Manoj Kumar <manoj@Manojs-MacBook-Air.local>
2024-10-09 11:50:52 +0100

    Fixed bug/Added new feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ban1 from '../assets/ban3.jpg'
+import Resume from '../assets/Nithya.pdf'
 
 
 const Home = () => {
@@ -12,7 +13,7 @@ const Home = () => {
                         <p className='text-shadow-red sm:text-4xl  md:text-5xl lg:text-6xl text-white font-header'>I am React Developer</p>
                     </h1>
                     <div className='flex flex-row mt-4'>
-                        <a href="/Resume3.pdf" target="_blank" >
+                        <a href={Resume} target="_blank" download>
                             <button className="bg-yellow-500 text-white font-bold  sm:px-2 sm:py-1 sm:px-2 md:py-2 md:px-4 rounded hover:bg-blue-500 mr-5 font-nav">Download Resume</button>
                         </a>
                         <a href="#project" className="bg-yellow-500 text-white font-bold sm:px-2 sm:py-1 sm:px-2 md:py-2 md:px-4 rounded hover:bg-blue-500 font-nav">
